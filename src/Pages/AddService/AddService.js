a423fc7e90aa89c912b3cac33abc854fe0c8a72f
DebabrataSaha-570 <sahadebabrata570@gmail.com>
2021-11-25 02:39:34 +0600

    update

--- src/Pages/AddService/AddService.js
+++ src/Pages/AddService/AddService.js
@@ -1,9 +1,22 @@
 import React from 'react';
-
+import { useForm } from "react-hook-form";
+import './AddService.css'
 const AddService = () => {
+    const { register, handleSubmit } = useForm();
+    const onSubmit = data => {
+        console.log(data)
+    };
+
     return (
-        <div>
-            <h2>Add A service</h2>
+        <div className="add-service">
+            <h2>Please Add A service</h2>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input  {...register("name", { required: true, maxLength: 20 })} placeholder="name" />
+                <textarea {...register("description")} placeholder="description " />
+                <input type="number" {...register("price",)} placeholder="price" />
+                <input {...register("img")} placeholder="image URL" />
+                <input type="submit" />
+            </form>
         </div>
     );
 };
