aebc38f142992bdcb84e11e21044afa3a73dd845
DebabrataSaha-570 <sahadebabrata570@gmail.com>
2021-11-25 02:05:29 +0600

    update

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import Login from './Pages/Login/Login/Login';
 import Header from './Pages/Shared/Header/Header';
 import AuthProvider from './Context/AuthProvider';
 import PrivateRoute from './Pages/Login/PrivateRoute/PrivateRoute';
+import AddService from './Pages/AddService/AddService';
 function App() {
   return (
     <div className="App">
@@ -35,6 +36,9 @@ function App() {
             </PrivateRoute>
             <Route path="/login">
               <Login></Login>
+            </Route>
+            <Route path="/addService">
+              <AddService></AddService>
             </Route>
             <Route path="*">
               <NotFound></NotFound>
